3faa49957c99a85cf6ce0a6df1314d04e431e7f8
AnthonyOgamba <anthonynwachukwu.ogamba@edu.sait.ca>
2026-01-22 15:51:08 -0700

    Week 2 page and homepage link

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Anthony Ogamba</p>
+      <Link href="https://github.com/AnthonyOgamba/cprg306-assignments">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+
+      <p>
+        <Link href="/week-2">Week 2 - Shopping List</Link>
+      </p>
+
+      <p>This site contains weekly assignments for CPRG 306.</p>
+    </main>
+  );
+}
